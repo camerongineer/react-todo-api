f3ce20e1bd3efb9ce4347569ade5f51780f13cd6
Cameron Marr <alphaomegabeta@gmail.com>
2023-11-09 09:41:14 -0800

    Load variables from process.env

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
-const dotenv = require("dotenv").config();
 const app = express();
+require("dotenv").config();
 
 const AIRTABLE_URL = "https://api.airtable.com/v0";
-const BASE_ID = dotenv.parsed.AIRTABLE_BASE_ID;
+const BASE_ID = process.env.AIRTABLE_BASE_ID;
 const AIRTABLE_TABLE_NAME = "todolist";
 const SORT_BY_LAST_MODIFIED_TIME = "?sort%5B0%5D%5Bfield%5D=lastModifiedTime&sort%5B0%5D%5Bdirection%5D=asc";
-const PORT = dotenv.parsed.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const fullUrl = `${AIRTABLE_URL}/${BASE_ID}/${AIRTABLE_TABLE_NAME}${SORT_BY_LAST_MODIFIED_TIME}`;
 
@@ -28,7 +28,7 @@ app.get("/status", (request, response) => {
 app.get("/items", (request, response) => {
     axios.get(fullUrl, {
         headers: {
-            "Authorization": `Bearer ${dotenv.parsed.AIRTABLE_API_KEY}`
+            "Authorization": `Bearer ${process.env.AIRTABLE_API_KEY}`
         }
     }).then(res => response.send(res.data["records"] ?? [])).catch(err => console.log(err));
 });
@@ -36,7 +36,7 @@ app.get("/items", (request, response) => {
 app.post("/items", (request, response) => {
     axios.post(fullUrl, request.body, {
         headers: {
-            "Authorization": `Bearer ${dotenv.parsed.AIRTABLE_API_KEY}`,
+            "Authorization": `Bearer ${process.env.AIRTABLE_API_KEY}`,
             "Content-Type": "application/json"
         }
     }).then(res => response.send(res.data)).catch(err => console.log(err));
